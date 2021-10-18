8cfbf14afb30a7b9ca0065a69777cc4363e2f115
Júlia Beltran <juliamirandabeltran@gmail.com>
2021-10-18 18:38:04 -0300

    finalizando

--- src/main/java/br/com/episteme/facilita/service/ServiceSimulado.java
+++ src/main/java/br/com/episteme/facilita/service/ServiceSimulado.java
@@ -55,7 +55,7 @@ public class ServiceSimulado {
         ArrayList<Questao> selecionadas = new ArrayList<>();
         for (Disciplina disciplina : disciplinas) {
             List<Questao> todas = questaoRepository.findByDisciplinaAndTipoDeProva(disciplina, tipoDeProva);
-            selecionadas.add(todas.get(0));
+            selecionadas.add(todas.get(1));
         }
         Simulado simuladoDiagnostico = new Simulado(1L, selecionadas);
         simuladoRepository.save(simuladoDiagnostico);
